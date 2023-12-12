365179ffa603ff0c0c474cdec806e2f6517c1a1b
abdelrahman27104 <112351005+abdelrahman27104@users.noreply.github.com>
2023-12-12 17:04:01 +0200

    Pyramic Tic-Tac-Toe_App.cpp

---
+++ Pyramic Tic-Tac-Toe_App.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include"Pyramic Tic-Tac-Toe.cpp"
+using namespace std;
+
+int main() {
+    
+ int choice;
+    Player* players[2];
+    players[0] = new Player (1, 'x');
+
+    cout << "Welcome to FCAI X-O Game. :)\n";
+    cout << "Press 1 if you want to play with computer: ";
+    cin >> choice;
+    if (choice != 1)
+        players[1] = new Player (2, 'o');
+    else
+    //Player pointer points to child
+        players[1] = new RandomPlayer ('o', 3);
+
+    GameManager x_o_game (new X_O_Board(), players);
+    x_o_game.run();
+    system ("pause");
+}
